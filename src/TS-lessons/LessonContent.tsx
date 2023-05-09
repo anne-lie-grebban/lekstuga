3d0f59d46dfc9ea616a4f3fd148102f4520ca1da
anne-lie-grebban <71642100+anne-lie-grebban@users.noreply.github.com>
2023-05-09 12:44:30 +0200

    feat(Lessons): TS lesson 14 - useReducer

--- src/TS-lessons/LessonContent.tsx
+++ src/TS-lessons/LessonContent.tsx
@@ -1,12 +1,13 @@
 //import Lesson12 from "./lesson12-React-TS/Lesson12";
 //import Lesson13 from "./lesson13-TS-react-hooks/Lesson13";
-import Lesson14 from "./lesson14-useReducer-useContext/Lesson14";
+//import Lesson14 from "./lesson14-useReducer/Lesson14";
+import Lesson15 from "./lesson15-useContext/Lesson15";
 
 const LessonContent = () => {
   return (
     <div className='main-tutorial'>
       <div className='tutorial-content-container'>
-        <Lesson14 />
+        <Lesson15 />
       </div>
     </div>
   );
